5c1c6386090a7ff74884759b77a3ba58fe2e331c
Richard Minsoo <67932257+RichardMinsooGo-RL-Gym@users.noreply.github.com>
2020-09-04 07:11:30 +0900

    Add files via upload

--- 22_Type_B_TF_pendulum/02_type_b_pendulum_NIPS2013_GREEN.py
+++ 22_Type_B_TF_pendulum/02_type_b_pendulum_NIPS2013_GREEN.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
-import random
+import random as ran
 import numpy as np
 import time, datetime
 from collections import deque
-import dqn
 import gym
 import pylab
 import sys
@@ -38,144 +37,156 @@ epsilon_min = 0.0001
 epsilon_decay = 0.0001
 
 hidden1 = 256
-update_cycle = 10
-
 memory = []
 size_replay_memory = 50000
 batch_size = 64
 
-def train_model(agent: dqn.DQN, minibatch: list) -> float:
-    states      = np.vstack([batch[0] for batch in minibatch])
-    actions     = np.array( [batch[1] for batch in minibatch])
-    rewards     = np.array( [batch[2] for batch in minibatch])
-    next_states = np.vstack([batch[3] for batch in minibatch])
-    dones       = np.array( [batch[4] for batch in minibatch])
+X = tf.placeholder(dtype=tf.float32, shape=(None, state_size), name="input_X")
+Y = tf.placeholder(dtype=tf.float32, shape=(None, action_size), name="output_Y")
+dropout = tf.placeholder(dtype=tf.float32)
 
-    X_batch = states
-    q_value = agent.predict(states)
+w_fc1 = tf.get_variable('w_fc1',shape=[state_size, hidden1]
+                        ,initializer=tf.contrib.layers.xavier_initializer())
+w_output = tf.get_variable('w_output',shape=[hidden1, action_size]
+                        ,initializer=tf.contrib.layers.xavier_initializer())
 
-    y_array = rewards + discount_factor * np.max(agent.predict(next_states), axis=1) * ~dones
+b_fc1 = tf.Variable(tf.zeros([1],dtype=tf.float32))
 
-    q_value[np.arange(len(X_batch)), actions] = y_array
+_h_fc1 = tf.nn.relu(tf.matmul(X,w_fc1)+b_fc1)
+h_fc1 = tf.nn.dropout(_h_fc1,dropout)
+output = tf.matmul(h_fc1,w_output)
 
-    Loss, _ = agent.update(X_batch, q_value)
+Loss = tf.reduce_sum(tf.square(Y-output))
+optimizer = tf.train.AdamOptimizer(learning_rate, epsilon=0.01)
+train = optimizer.minimize(Loss)
 
-    return Loss
+memory = deque(maxlen=size_replay_memory)
+progress = " "
 
-def main():
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    sess.run(init)
 
-    memory = deque(maxlen=size_replay_memory)
-    progress = " "
+    avg_score = -120
+    episode = 0
+    episodes, scores = [], []
+    epsilon = epsilon_max
+    start_time = time.time()
+    
+    while time.time() - start_time < 10*60 and avg_score < -15: 
 
-    with tf.Session() as sess:
-        agent = dqn.DQN(sess, state_size, action_size, name="main")
-        init = tf.global_variables_initializer()
-        saver = tf.train.Saver()
-        sess.run(init)
+        state = env.reset()
+        score = 0
+        done = False
+        ep_step = 0
+        score = 0
 
-        avg_score = -120
-        episode = 0
-        episodes, scores = [], []
-        epsilon = epsilon_max
-        start_time = time.time()
+        while not done and ep_step < 200 :
 
-        while time.time() - start_time < 10*60 and avg_score < -15:
-            
-            state = env.reset()
-            score = 0
-            done = False
-            ep_step = 0
-            score = 0
-            
-            while not done and ep_step < 200:   
+            if len(memory) < size_replay_memory:
+                progress = "Exploration"            
+            else:
+                progress = "Training"
 
-                if len(memory) < size_replay_memory:
-                    progress = "Exploration"            
-                else:
-                    progress = "Training"
+            #env.render()
+            ep_step += 1
 
-                #env.render()
-                ep_step += 1
-                
-                if epsilon > np.random.rand(1):
-                    # action = env.action_space.sample()
-                    action = np.random.randint(0, agent.action_size)
+            state = np.reshape(state,[1,state_size])
+            q_value = sess.run(output, feed_dict={X:state, dropout: 1})
 
-                else:
-                    actions_value = agent.predict(state)
-                    action = np.argmax(actions_value)
-
-                f_action = (action-(action_size-1)/2)/((action_size-1)/4)
-                # print(f_action)
-                next_state, reward, done, _ = env.step(np.array([f_action]))
-                
-                reward /= 10
-                score += reward                
-                
-                memory.append((state, action, reward, next_state, done))
-
-                if len(memory) > size_replay_memory:
-                    memory.popleft()
-                
-                if progress == "Training":
-                    minibatch = random.sample(memory, batch_size)
-                    train_model(agent, minibatch)
-                    
-                    if epsilon > epsilon_min:
-                        epsilon -= epsilon_decay
-                    else:
-                        epsilon = epsilon_min
-
-                state = next_state
-
-                if done or ep_step == 200:
-                    if progress == "Training":
-                        episode += 1
-                        scores.append(score)
-                        episodes.append(episode)
-                        avg_score = np.mean(scores[-min(30, len(scores)):])
-
-                    print("episode {:>5d} / score:{:>5.1f} / recent 30 game avg:{:>5.1f} / epsilon :{:>1.5f}"
-                              .format(episode, score, avg_score, epsilon))            
-                    break
-
-        save_path = saver.save(sess, model_path + "/model.ckpt")
-        print("\n Model saved in file: %s" % save_path)
-
-        pylab.plot(episodes, scores, 'b')
-        pylab.savefig(graph_path + "/pendulum_NIPS2013.png")
-
-        e = int(time.time() - start_time)
-        print(' Elasped time :{:02d}:{:02d}:{:02d}'.format(e // 3600, (e % 3600 // 60), e % 60))
-
-    # Replay the result
-        episode = 0
-        scores = []
-        while episode < 20:
-            
-            state = env.reset()
-            done = False
-            ep_step = 0
-            score = 0
-            
-            while not done and ep_step < 200:
-                env.render()
-                ep_step += 1
-                q_value = agent.predict(state)
+            if epsilon > np.random.rand(1):
+                # action = env.action_space.sample()
+                action = np.random.randint(0, action_size)
+            else:
                 action = np.argmax(q_value)
-                
-                f_action = (action-(action_size-1)/2)/((action_size-1)/4)
-                # print(f_action)
-                next_state, reward, done, _ = env.step(np.array([f_action]))
-                
-                reward /= 10
-                score += reward
-                state = next_state
-                
-                if done or ep_step == 200:
+
+            f_action = (action-(action_size-1)/2)/((action_size-1)/4)
+            # print(f_action)
+            next_state, reward, done, _ = env.step(np.array([f_action]))
+
+            reward /= 10
+            score += reward
+
+            memory.append([state, action, reward, next_state, done, ep_step])
+
+            if len(memory) > size_replay_memory:
+                memory.popleft()
+
+            if progress == "Training":
+                for minibatch in ran.sample(memory, batch_size):
+                    states, actions, rewards, next_states, dones ,ep_steps = minibatch
+                    q_value = sess.run(output, feed_dict={X: states, dropout: 1})
+
+                    if dones:
+                        if ep_steps < env.spec.timestep_limit :
+                            q_value[0, actions] = -100
+                    else:
+                        next_states = np.reshape(next_states,[1,state_size])
+                        q_value_next = sess.run(output, feed_dict={X: next_states, dropout: 1})                    
+                        q_value[0, actions] = rewards + discount_factor * np.max(q_value_next)
+
+                    _, loss = sess.run([train, Loss], feed_dict={X: states, Y: q_value, dropout:1})
+
+                if epsilon > epsilon_min:
+                    epsilon -= epsilon_decay
+                else:
+                    epsilon = epsilon_min
+
+            state = next_state
+
+            if done or ep_step == 200:
+                if progress == "Training":
                     episode += 1
                     scores.append(score)
-                    print("episode : {:>5d} / score : {:>5.1f} / avg reward : {:>5.1f}".format(episode, score, np.mean(scores)))
+                    episodes.append(episode)
+                    avg_score = np.mean(scores[-min(30, len(scores)):])
 
-if __name__ == "__main__":
-    main()
+                print("episode {:>5d} / score:{:>5.1f} / recent 30 game avg:{:>5.1f} / epsilon :{:>1.5f}"
+                          .format(episode, score, avg_score, epsilon))            
+                break
+
+    save_path = saver.save(sess, model_path + "/model.ckpt")
+    print("\n Model saved in file: %s" % save_path)
+
+    pylab.plot(episodes, scores, 'b')
+    pylab.savefig(graph_path + "/pendulum_NIPS2013.png")
+
+    e = int(time.time() - start_time)
+    print(' Elasped time :{:02d}:{:02d}:{:02d}'.format(e // 3600, (e % 3600 // 60), e % 60))
+
+# Replay the result
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    saver.restore(sess, model_path+ "/model.ckpt")
+    print("Play Cartpole!")
+    
+    episode = 0
+    scores = []
+    
+    while episode < 20:
+        
+        state = env.reset()
+        done = False
+        ep_step = 0
+        score = 0
+        
+        while not done and ep_step < 200:
+            # Plotting
+            env.render()
+            ep_step += 1
+            state = np.reshape(state, [1, state_size])
+            q_value = sess.run(output, feed_dict={X:state, dropout: 1})
+            action = np.argmax(q_value)
+            f_action = (action-(action_size-1)/2)/((action_size-1)/4)
+            # print(f_action)
+            next_state, reward, done, _ = env.step(np.array([f_action]))
+            reward /= 10
+            score += reward
+            state = next_state
+            
+            if done or ep_step == 200:
+                episode += 1
+                scores.append(score)
+                print("episode : {:>5d} / score : {:>5.1f} / avg reward : {:>5.1f}".format(episode, score, np.mean(scores)))
